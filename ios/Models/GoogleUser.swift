518823ffbaa813c66b9cbf3c54ea7a217a96e6d6
lindsay-cheng <lindsaycheng2@gmail.com>
2026-01-10 16:57:22 -0500

    google user struct jt #affirm

---
+++ ios/Models/GoogleUser.swift
@@ -0,0 +1,14 @@
+//
+//  GoogleUser.swift
+//  ios
+//
+//  Created by Lindsay Cheng on 2026-01-10.
+//
+
+import Foundation
+
+struct GoogleUser {
+    var firstName: String
+    var lastName: String
+    var email: String
+}

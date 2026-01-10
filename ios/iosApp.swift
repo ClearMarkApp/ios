223ce801a69dd6801afb3a4dd323b281f033aac3
Lucian Cheng <luciancheng3@gmail.com>
2026-01-10 11:14:23 -0500

    Initial Commit

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Lucian Cheng on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

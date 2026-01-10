ff7d37406240fb493047a7f83c6c60d0841017e2
Lucian Cheng <luciancheng3@gmail.com>
2026-01-10 13:47:46 -0500

    added build dependencies, assets, and set up http response

---
+++ ios/Managers/HTTPSetup.swift
@@ -0,0 +1,75 @@
+//
+//  HTTPSetup.swift
+//  ios
+//
+//  Created by Lucian Cheng on 2026-01-10.
+//
+
+import Foundation
+
+enum HTTPMethod : String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+struct APIError : Error {
+    let message : String
+}
+
+struct GenericRequestResponse : Decodable {
+    let message : String
+}
+
+struct AnyEncodable: Encodable {
+    private let encodeFunc: (Encoder) throws -> Void
+
+    init<T: Encodable>(_ wrapped: T) {
+        self.encodeFunc = wrapped.encode
+    }
+
+    func encode(to encoder: Encoder) throws {
+        try encodeFunc(encoder)
+    }
+}
+
+final class APIClient {
+    static let shared = APIClient()
+    private init() {}
+
+    private let baseURL = URL(string: "https://clearmark-4so0.onrender.com")!
+    private let apiKey = "nob"
+
+    func request<T: Decodable>(
+        path: String,
+        method: HTTPMethod,
+        body: Encodable? = nil
+    ) async throws -> T {
+        var request = URLRequest(url: baseURL.appendingPathComponent(path))
+        request.httpMethod = method.rawValue
+        
+        // standard headers
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
+
+        if let body = body {
+            let encoder = JSONEncoder()
+            encoder.dateEncodingStrategy = .iso8601
+            request.httpBody = try encoder.encode(AnyEncodable(body))
+        }
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let http = response as? HTTPURLResponse,
+              200..<300 ~= http.statusCode else {
+            throw APIError(message: "Invalid response")
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return try decoder.decode(T.self, from: data)
+    }
+}
